db586c3288f1a423684f7abf7ab2c3f30d5cbe5b
Noah Clouthier <noahclouthier@Noahs-MacBook-Air.local>
2023-02-08 16:28:25 -0800

    completed linkedlist tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+    if (!list.head) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var newHead = list.head.next;
+    if (!list.head) {
+      return;
+    }
+    var removed = list.head.value;
+    list.head = newHead;
+    return removed;
   };
 
   list.contains = function(target) {
+    var current = list.head;
+    while (current) {
+      if (current.value === target) {
+        return true;
+      } else {
+        current = current.next;
+      }
+    }
+    return false;
   };
 
   return list;
